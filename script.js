c61765006c15df524c1cca1b91945c6910671bf9
Yumi <ydshiroma@gmail.com>
2023-07-23 14:20:03 -0400

    completed tasks up until Fetching Planetary Data

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    const list = [];
+    const pilot = document.getElementById("pilotName");
+    const copilot = document.querySelector("input[name=copilotName]");
+    const fuelLevel = document.querySelector("input[name=fuelLevel]");
+    const cargoMass = document.querySelector("input[name=cargoMass]");
+
+    console.log(pilot.value);
+    console.log(copilot.value);
+    console.log(fuelLevel.value);
+    console.log(cargoMass.value);
+    console.log("console log complete");
+
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
+  })
+  // set up variables for document, list, pilot, copilot, fuelLevel, cargoLevel
+  // use variables to call formSubmission
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -12,5 +30,5 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-   
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,66 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   // validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate.
+   if (testInput == "") {
+     return "Empty";
+   } else if (isNaN(Number(testInput))) {
+     return "Not a Number";
+   } else {
+     return "Is a Number";
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+  const correctValues = {
+    pilot: "Not a Number",
+    copilot: "Not a Number",
+    fuelLevel: "Is a Number",
+    cargoLevel: "Is a Number"
+  }
+
+  if (correctValues.pilot == validateInput(pilot.value) && correctValues.copilot == validateInput(copilot.value) && correctValues.fuelLevel == validateInput(fuelLevel.value) && correctValues.cargoLevel == validateInput(cargoLevel.value)) {
+    document.querySelector("div[id=faultyItems]").style.visibility = "visible";
+
+    document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot.value} is ready for launch`;
+    document.getElementById("copilotStatus").innerHTML = `Copilot ${copilot.value} is ready for launch`
+
+    let launchReady = true;
+
+    if ( Number(fuelLevel.value) < 10000) {
+      // Fuel level too low for launch
+      document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+      launchReady = false;
+    } else {
+      document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch";
+    };
+
+    if ( Number(cargoLevel.value) > 10000) {
+      // Cargo mass too heavy for launch
+      document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+      launchReady = false;
+    } else {
+      document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch";
+    };
+
+    let launchStatus = document.getElementById("launchStatus");
+
+    if (launchReady == true) {
+      launchStatus.innerHTML = "Shuttle is ready for launch";
+      launchStatus.style.color = "#419F6A";
+    } else {
+      launchStatus.innerHTML = "Shuttle not ready for launch";
+      launchStatus.style.color = "#C7254E";
+    }
+
+    event.preventDefault();
+  } else {
+    alert("Make sure to enter valid information for each field!");
+    event.preventDefault();
+  }
+  // return;
+
+// Using the input values, update the shuttle requirements.
 }
 
 async function myFetch() {
@@ -39,5 +94,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
